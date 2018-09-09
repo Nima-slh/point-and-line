157d89db3a815b7904453722528c846fd0f8d34e
Nima-slh <42717701+Nima-slh@users.noreply.github.com>
2018-09-09 18:34:21 +0430

    Add files via upload

---
+++ line.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using point_and_line;
+
+namespace point_and_line
+{
+    public class line
+    {
+
+        public point StartPoint { get; set; }
+        public point EndPoint { get; set; }
+        public line()
+        {
+            StartPoint =new point();
+            EndPoint = new point();
+        }
+        public line(point startPoint, point endPoint)
+        {
+            this.StartPoint = startPoint;
+            this.EndPoint = endPoint;
+        }
+        public int GetSPX()
+        {
+            return this.StartPoint.getX();
+        }
+        public void SetSPX(int x)
+        {
+            this.StartPoint.setX(x);
+        }
+        public int GetSPY()
+        {
+            return this.StartPoint.getY();
+        }
+        public void SetSPY(int y)
+        {
+            this.StartPoint.setY(y);
+        }
+        public int GetEPX()
+        {
+            return this.EndPoint.getX();
+        }
+        public void SetEPX(int x)
+        {
+            this.EndPoint.setX(x);
+        }
+        public int GetEPY()
+        {
+            return this.EndPoint.getY();
+        }
+        public void SetEPY(int y)
+        {
+            this.EndPoint.setY(y);
+        }
+        /// <summary>
+        /// طول خط
+        /// </summary>
+        /// <param name="l"></param>
+        /// <returns></returns>
+        public static Double lengh(line l)
+        {
+            return point.distance(l.StartPoint,l.EndPoint);
+        }
+        /// <summary>
+        /// شیب خط
+        /// </summary>
+        /// <param name="l"></param>
+        /// <returns></returns>
+        public static Double slope(line l)
+        {
+            return (l.EndPoint.getY() - l.StartPoint.getY()) / (l.EndPoint.getX() - l.StartPoint.getX());
+        }
+        public override string ToString()
+        {
+            return $"startpoint: x = {this.GetSPX()} y = {this.GetSPY()} /n endpoint: x = {this.GetEPX()} y = {this.GetEPY()}";
+        }
+    }
+}
---
+++ point.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace point_and_line
+{
+    public class point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public String Color { get; set; }
+        public point()
+        {
+            this.X = 0;
+            this.Y = 0;
+        }
+        public point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public point(int x, int y, string color) : this(x, y)
+        {
+            this.Color = color;
+        }
+        public int getX()
+        {
+            return X;
+        }
+        public void setX(int x)
+        {
+            this.X = x;
+        }
+        public int getY()
+        {
+            return Y;
+        }
+        public void setY(int y)
+        {
+            this.Y = y;
+        }
+        /// <summary>
+        /// فاصله دو نقطه
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static double distance(point a, point b)
+        {
+            int XDis = a.X - b.X;
+            int YDis = a.Y - b.Y;
+            return Math.Abs(Math.Sqrt(XDis * XDis + YDis * YDis));
+        }
+        /// <summary>
+        /// فاصله از مبدا
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static double distance(point a)
+        {
+            point b = new point();
+            return distance(a, b);
+        }
+       
+    }
+}
